06d0d0c0c6bead83142d580f6c454a10f28280f7
qiruwa <C:\Users\anohin\AppData\Roaming\The Bat!>
2022-06-10 14:29:10 +0300

    first

--- task01-arrays.go
+++ task01-arrays.go
@@ -1,6 +1,10 @@
 package homework
 
 func average(input [15]float32) (result float32) {
-	//Place your code here
-	return
+    sum := 0
+	for _, a := range input {
+	    sum = sum + a
+	}
+	average = sum / len(input)
+	return average
 }
--- task03-map.go
+++ task03-map.go
@@ -1,6 +1,21 @@
 package homework
 
 func sortMapValues(input map[int]string) (result []string) {
-	//Place your code here
-	return
+	keys := []int{}
+	values := []string{}
+	for key := range input{
+	    keys = append(keys, key)
+	}
+	for i := 0; i < len(keys)-1; i++ {
+		for j := 0; j < len(keys)-i-1; j++ {
+			if keys[j] > keys[j+1] {
+				keys[j], keys[j+1] = keys[j+1], keys[j]
+			}
+		}
+	}
+    for _, k := range keys {
+		values = append(values, basket[k])
+	}
+
+	return values
 }
