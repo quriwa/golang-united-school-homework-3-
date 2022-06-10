35167d01bd3fe1c95354114f96e65fe38c3b5ed1
qiruwa <C:\Users\anohin\AppData\Roaming\The Bat!>
2022-06-10 14:44:48 +0300

    second

--- task01-arrays.go
+++ task01-arrays.go
@@ -1,10 +1,9 @@
 package homework
 
 func average(input [15]float32) (result float32) {
-    sum := 0
+	var sum float32 = 0
 	for _, a := range input {
-	    sum = sum + a
+		sum += a
 	}
-	average = sum / len(input)
-	return average
+	return sum / float32(len(input))
 }
